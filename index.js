243109a73288d01d707c5090b8bf3bd68925be19
Jaidyn Martinez <jaidyn.amarti@gmail.com>
2022-05-25 13:30:29 -0500

    Jamie's first attack

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
+// Jon's stats
 let jonSnowAttack = 25;
+let jonSnowDefense = 0;
+let jonSnowHealth = 100;
+// Jamie's stats
 let jamieLannisterAttack = 25;
 
 if (jonSnowAttack > jamieLannisterAttack){
@@ -7,4 +11,11 @@ if (jonSnowAttack > jamieLannisterAttack){
     console.log("Jamie Lannister has better attack than Jon Snow");
 } else{
     console.log("Jon Snow and Jamie Lannister have the same attack");
+}
+
+if (jonSnowHealth <= jamieLannisterAttack){
+    console.log("Jon Snow has been slain");
+} else {
+    jonSnowHealth -= jamieLannisterAttack;
+    console.log(`Jon Snow's health is down to ${jonSnowHealth}`);
 }
